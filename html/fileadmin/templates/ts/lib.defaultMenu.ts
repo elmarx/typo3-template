89602d496b2ff7665b6b86b6fd916fdf911e9ce5
Elmar Athmer <elmar@nixus-minimax.de>
2011-01-24 15:38:14 +0100

    - add a default menu object and use it in the main Menu

---
+++ html/fileadmin/templates/ts/lib.defaultMenu.ts
@@ -0,0 +1,35 @@
+lib.defaultMenu = HMENU
+lib.defaultMenu {
+	1 = TMENU
+	1 {
+		noBlur = 1
+		wrap = <ul>|</ul>
+		NO = 1
+		NO.wrapItemAndSub = <li>|</li>
+		
+		ACT = 1
+		ACT.wrapItemAndSub = <li class="active">|</li>
+
+		CUR = 1
+		CUR.wrapItemAndSub = <li class="current">|</li>
+		
+		expAll = 1
+	}
+	
+	2 = TMENU
+	2 {
+		noBlur = 1
+		wrap = <ul>|</ul>
+		NO = 1
+		NO.wrapItemAndSub = <li>|</li>
+
+		ACT = 1
+		ACT.wrapItemAndSub = <li class="active">|</li>
+
+		CUR = 1
+		CUR.wrapItemAndSub = <li class="current">|</li>
+	
+		expAll = 1
+	}
+
+}
