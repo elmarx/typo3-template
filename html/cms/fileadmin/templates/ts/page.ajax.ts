c8011763dc1b7a99decf73c38916748797c314cc
Elmar Athmer <elmar@nixus-minimax.de>
2011-03-08 15:12:52 +0100

    - ajax pagetype

---
+++ html/cms/fileadmin/templates/ts/page.ajax.ts
@@ -0,0 +1,16 @@
+ajax = PAGE 
+ajax {
+	typeNum = 3
+	
+	config {
+		disableAllHeaderCode = 1
+		xhtml_cleaning = 0
+	}
+	
+	10 < styles.content.get
+
+}
+
+[globalVar = GP:type = 3]
+	tt_content.stdWrap.innerWrap >
+[global]
